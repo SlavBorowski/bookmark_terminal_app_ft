83996c84c2c33fec23ad7381238d58e7118522a2
Harrison Malone <harrison.malone2@gmail.com>
2020-09-21 12:25:00 +1000

    added bookmark helper and the user input class method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,30 +1,24 @@
+require_relative 'bookmark_helper'
+
 class Bookmark
-  # getter
   attr_reader :tags, :title, :url, :description
 
-	def initialize(title, url, tags, description) 
-		@title = title
-		@url = url
-		@tags = tags
-		@description = description
+  include BookmarkHelper
+
+  def initialize(title, url, tags, description)
+    @title = title
+    @url = url
+    @tags = tags
+    @description = description
+  end
+
+  def self.bookmark_user_input
+    bookmark = {}
+    INPUTS.each do |input|
+      puts "Whats the #{input}?"
+      print '> '
+      bookmark[input] = gets.chomp
+    end
+    bookmark
   end
 end
-
-# bookmark = Bookmark.new(
-# 	"coder academy", 
-# 	"https://coderacademy.edu.au", 
-# 	["code", "learn"],
-# 	"this is a place i want to study at!"
-# )
-
-# access attributes on the outside
-# getter
-# puts bookmark.description
-
-# writing to attributes
-# setter
-
-# puts "Whats your new description?"
-# print "> "
-# bookmark.description = gets.chomp
-# puts bookmark.description
