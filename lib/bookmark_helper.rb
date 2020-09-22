f9cfedad2ef79981dc4a3417ddfcc40bb9091cdd
Harrison Malone <harrison.malone2@gmail.com>
2020-09-22 12:39:13 +1000

    added bookmark file path constant

--- lib/bookmark_helper.rb
+++ lib/bookmark_helper.rb
@@ -1,4 +1,5 @@
 module BookmarkHelper
   INPUTS = %i[title url tags description].freeze
   HEADINGS = %i[id title url tags description].freeze
+  BOOKMARKS_DATABASE = Dir.pwd + "/public/bookmarks.json"
 end
